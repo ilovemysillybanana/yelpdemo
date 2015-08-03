661696feece51e5e233584ce34932963d617bdb1
Jose <ilovemysillybanana@gmail.com>
2015-08-03 17:09:05 -0400

    Added region to carrier wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["aws_access_key_id"],
-    aws_secret_access_key: ENV["aws_secret_access_key"]
+    aws_secret_access_key: ENV["aws_secret_access_key"],
+    region: 'us-west-2'
   }
   config.fog_directory  = 'name_of_directory'                          # required
 end
